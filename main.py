0c0c73577e761249a75eeeaaacaa40d2388125e2
sk.abdul haq <75989562+skabdulhaq@users.noreply.github.com>
2021-11-27 12:58:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ reg = 125004384
 sas_id = "125004384@sastra.ac.in"
 describe = """This Form Is Filled By Python!!!
 I like Developing, My  daily routine was used to be Eat, Code, Sleep, Repeat before November 23 Because CIA Schedule released. Since I was a child I used to be very passionate about learning new technology's. I am presently a intermediate python programmer Done projects using beautifulsoup, Selenium,  Leaning Flask, JavaScript and Flutter. If I get a chance to explore video editing I will definitely do it too. I am a fast learner I can  learn new technology quickly, so I am very much confident that I will adopt to your environment very quickly. So, please check projects on GitHub. Thankyou for reading in ADVANCE!!"""
-# browser.quit()
-projects = """300dpi form filler : https://github.com/skabdulhaq/Password_Manager
+
+projects = """300dpi form filler : https://github.com/skabdulhaq/300dpi_form_filler
+Portfolio Repository: https://github.com/skabdulhaq/Future-Portfolio/
+Portfolio: https://skabdulhaq.github.io/Future-Portfolio/
+India Map Game: https://github.com/skabdulhaq/India-Map-Game
+Auto insta following: https://github.com/skabdulhaq/Auto_Insta_Following_Bot/
+Movie links scraper: https://github.com/skabdulhaq/Movie_links_scraper
 Password Manager: https://github.com/skabdulhaq/Password_Manager
 KBC: https://github.com/skabdulhaq/KBC
-Movie links scraper: https://github.com/skabdulhaq/Movie_links_scraper
 """
 email = browser.find_element(by.By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div[1]/input')
 email.send_keys(email_)
